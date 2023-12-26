c4f003b1f2daacf82eda4ec4b57ae57fbc355ced
imananuar <imananuar5367@gmail.com>
2023-12-26 22:00:18 +0800

    Store refreshToken in the cookies and set expiry time

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,13 +10,14 @@ import { useRouter } from "next/router";
 import { HttpResponse } from "@/dto/http-response.dto";
 import { LoginResponse } from "@/dto/auth.dto";
 import { useCookies } from 'react-cookie';
+import { jwtDecode } from "jwt-decode";
 
 
 export default function Login() {
     const form = useForm<FieldValues>();
     const { register, control, handleSubmit, formState } = useForm<FieldValues>();
     const router = useRouter();
-    const [cookies, setCookie] = useCookies(['access_token']);
+    const [cookies, setCookie] = useCookies(["access_token", "refresh_token"]);
 
     useEffect(() => {
         !!cookies.access_token ? router.push("/projects/main") : null
@@ -27,12 +28,23 @@ export default function Login() {
         { ssr: false }
     );
 
+    const dateFormat = (timestamp: number | undefined) => {
+
+        if (typeof(timestamp) === "number") {
+            // Create a new Date object using the milliseconds
+            const date = new Date(timestamp * 1000 * 1000);
+        }
+    }
+
     const login = useMutation({
         mutationFn: LoginAPI,
         onSuccess: (response: HttpResponse<LoginResponse>) => {
             if (response.statusCode === 201) {
-                setCookie('access_token', response.data.accessToken)
-                router.push("/projects/main");
+                const accessExpiryTime = new Date(jwtDecode(response.data.accessToken).exp * 1000 );
+                const refreshExpiryTime = new Date(jwtDecode(response.data.refreshToken).exp * 1000 );
+                setCookie('access_token', response.data.accessToken, { expires: accessExpiryTime })
+                setCookie('refresh_token', response.data.refreshToken, {expires: refreshExpiryTime })
+                router.replace("/projects/main");
             } else {
                 alert("Incorrect username / password. Please try again.");
             }
